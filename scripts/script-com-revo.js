59fbd08845e26bbb7c168cddb10fab9e3e83b1e6
soumy-22 <149955260+soumy-22@users.noreply.github.com>
2024-07-24 20:39:38 +0600

    Add files via upload

---
+++ scripts/script-com-revo.js
@@ -0,0 +1,43 @@
+
+function scaleMe2()
+{
+    console.log("interval check");
+    const annosa = document.getElementById('google-anno-sa');
+    const hostElements = document.querySelectorAll('div[style*="color-scheme: initial"][style*="forced-color-adjust: initial"][style*="mask: initial"][style*="math-depth: initial"]');
+    hostElements.forEach(hostElement => { alldynamic(hostElement, annosa); });
+
+    function alldynamic(hostElement, annosa)
+    {
+       if (hostElement.shadowRoot) 
+       {
+           const shadowdom = hostElement.shadowRoot;
+           const toolbar = shadowdom.getElementById('ft-floating-toolbar'); 
+           const contain = shadowdom.querySelector('.ipr-container'); 
+
+           if (toolbar || contain)
+           {
+               shadowdom.innerHTML = '';
+               shadowdom.host.remove();
+               console.log("111");
+           }
+       }
+
+       if (annosa && window.matchMedia("(min-width: 615px)").matches) 
+       {
+           annosa.style.removeProperty('display');
+           annosa.style.setProperty('display', 'none', 'important');
+           console.log("Display 222");
+       }
+       if (annosa && window.matchMedia("(max-width: 615px)").matches) 
+       {
+           annosa.style.removeProperty('display');
+           annosa.style.setProperty('display', 'none', 'important');
+           console.log("Display 333");
+       }
+    }
+}
+
+const ftinterval = setInterval(scaleMe2, 1000); scaleMe2(); 
+
+// document ends here -------
+
