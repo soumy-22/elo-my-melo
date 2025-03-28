b209ef625d2766a8051bc878bc37a78acc1934dd
soumy-22 <149955260+soumy-22@users.noreply.github.com>
2025-03-28 08:15:57 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Activate immediately after install
+  event.waitUntil(self.skipWaiting()); // Activate immediately after install
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim()); // Take control of clients immediately
 });
 
 self.addEventListener('push', event => {
@@ -13,20 +17,19 @@ self.addEventListener('push', event => {
     }
   }
 
-  // Use custom Flip 7 data as fallback if no valid payload
   const options = {
-    body: data.body || "Dive into the ultimate showdown between JBL Flip 7 and Flip 6 with 10 essential highlights that reveal their differences and upgrades. The Flip 6 introduced the iconic big, bold JBL logo, setting a cool, stylish tone, while the Flip 7 builds on this with a similar striking design.",
-    image: data.image || "https://elomymelo.com/story-images/jbl-flip-7v6/flip-7v6-01.jpg",
+    body: data.body || "This story compares the JBL Charge 6 and Flip 7 speakers! Explore their IP68 waterproof rating, powerful sound, 7-band EQ, and unique features like the Charge 6's power bank support and Flip 7's Auracast button. Find out which suits you best!",
+    image: data.image || "https://elomymelo.com/story-images/jbl-charge-6vf7/charge-6vf7-01.jpg",
     icon: data.icon || "https://elomymelo.com/old-images/circle-trans.png",
-    data: { url: data.url || "https://elomymelo.com/web-stories/flip-7v6-story.html" }
+    data: { url: data.url || "https://elomymelo.com/web-stories/charge-6vf7-story.html" },
+    tag: Date.now().toString() // Unique tag for each noti 
   };
 
-  // Trigger update and show notification
   event.waitUntil(
     Promise.all([
       self.registration.update(), // Check and install new sw.js in the background
       self.registration.showNotification(
-        data.title || "JBL Flip 7 vs Flip 6 | 10 Essential Highlights",
+        data.title || "JBL Charge 6 vs Flip 7 | Comparing top 5 features",
         options
       )
     ])
