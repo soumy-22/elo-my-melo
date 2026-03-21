e16df40cb6c9686aa877bc9741f1452d50fac399
soumy-22 <149955260+soumy-22@users.noreply.github.com>
2026-03-22 01:14:06 +0600

    Add files via upload

--- scripts/light-script.js
+++ scripts/light-script.js
@@ -132,6 +132,46 @@ function updateAdsAttributes()
 }
 updateAdsAttributes(); 
 
+// for detecting new news articles, h2 tags 
+const rplsSec = document.getElementById('lt-art-sec');
+const loadersvg = `<div class="loader-container"><svg viewBox="0 0 100 100" class="spinner-anim">
+<circle cx="50" cy="50" r="8" fill="#D1D1D1"/><path d="M 85.36 50 A 40 40 0 0 1 78.28 78.28" class="svg-arc" transform="rotate(10 50 50)"/>
+<path d="M 85.36 50 A 40 40 0 0 1 78.28 78.28" class="svg-arc" transform="rotate(130 50 50)"/>
+<path d="M 85.36 50 A 40 40 0 0 1 78.28 78.28" class="svg-arc" transform="rotate(250 50 50)"/>
+</svg></div>` // for loader html insertion 
+
+function ltrplsSec() 
+{
+    const loaderStyle = document.createElement('style');
+    loaderStyle.id = 'loader-style'; loaderStyle.textContent = '@keyframes spin{from{transform:rotate(0)}to{transform:rotate(360deg)}}.loader-container{width:64px;height:64px;position:absolute;inset:0;margin:auto}.spinner-anim{animation:spin 0.6s linear infinite;transform-origin:center}.svg-arc{stroke:#68ABD4;stroke-width:6;fill:none;stroke-linecap:round}';
+    document.head.appendChild(loaderStyle); if (rplsSec) { rplsSec.innerHTML = loadersvg; }
+
+    const ltArtSec = document.createElement('style'); ltArtSec.id = 'ltart-style';
+    ltArtSec.textContent = '.latest_articles{inset-inline:0;width:max-content;position:relative;margin:0 auto;top:30px;padding:15px 35px;font-family:monospace;font-size:15px;border-radius:55px;background-color:#f0f4f8;color:#567ea7;font-weight:bolder}#before-article-01{position:relative;margin:5px;height:2px}.articles-common-lt-style{height:auto;padding:0 20px}.latest-art-pic-common{width:100%;height:auto;border-radius:14px 14px 5px 5px;aspect-ratio:auto 16/9}.art-head-common{text-align:center;margin:0 auto;font-weight:bolder;font-family:sans-serif;padding:10px;border-radius:5px 5px 14px 14px;background-color:#f0f4f8;font-size:16px}.art-head-common a{color:#6487ab!important;text-decoration:none}.fixed-dis-phone{position:relative;height:auto;min-height:200px;display:flex;justify-content:center;padding:12px 4px;background:#EAEAEA;border-radius:14px;width:auto;margin:0 20px}#more-info-after-2{border:3px #EAEAEA solid;padding:20px 25px;text-align:center;font-family:sans-serif;width:73%;color:#6f6f6f;margin:0 auto 20px;font-size:17px;border-radius:20px}.art-description-common{display:none}.fixed-dis-desk{display:none}';
+    document.head.appendChild(ltArtSec); // for the latest article sec
+
+    fetch("https://elomymelo.com/text-files/inside-article-div.txt")
+    .then(response => response.text()).then(data => { if(rplsSec) { rplsSec.innerHTML = data; 
+    document.getElementById('loader-style')?.remove(); const w = window.innerWidth; // remove style 
+    const phoneAd = rplsSec.querySelector(".fixed-dis-phone"); const deskAd = rplsSec.querySelector(".fixed-dis-desk");
+    const script = document.createElement("script"); script.innerHTML = "(adsbygoogle = window.adsbygoogle || []).push({});";
+    if (w < 615) { deskAd?.querySelector("ins.adsbygoogle")?.remove(); phoneAd?.appendChild(script); } else {
+    phoneAd?.querySelector("ins.adsbygoogle")?.remove(); deskAd?.appendChild(script); } } });
+}
+
+// Run the function before intersection 
+if (window.innerWidth < 615) 
+{
+    const intObserver = new IntersectionObserver((entries) => { entries.forEach(entry => {
+    if (entry.isIntersecting) { ltrplsSec(); intObserver.disconnect(); } }); },
+    { rootMargin: '0px 0px 500px 0px', threshold: 0 } );
+    intObserver.observe(rplsSec);
+}
+
+// for go to top or bottom
+function goDown() { window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' }); }
+function goUp() { window.scrollTo({ top: 0, behavior: 'smooth' }); }
+
 // for last-line balance
 const artCon = document.querySelector('.art-out');
 function insertAndMeasureSpan(paraTag) 
